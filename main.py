69d268e1b44e017cf8d13d0b4bf2541894f6ac19
gautamkmahato <gautamkmahato@gmail.com>
2024-02-24 00:35:52 +0530

    created new POST endpoint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 import pikepdf
 
@@ -7,22 +8,40 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-def generatePassword():
+class User(BaseModel):
+    name: str
+    description: str
+    token: str
+    password: str 
+
+def generatePassword(name: str):
     oldPdf = pikepdf.Pdf.open('SamplePDF.pdf') 
     no_extracting = pikepdf.Permissions(extract=False)
-    password = "12345"
+    password = name
     outputFolder = './output/'
     outputFileName = 'encrypted.pdf'
     val = outputFolder + outputFileName
     print(val)
-    oldPdf.save(val, encryption=pikepdf.Encryption(
-        user=password, owner="owner password", allow=no_extracting
-    ))
+    try:
+        oldPdf.save(val, encryption=pikepdf.Encryption(
+            user=password, owner="owner password", allow=no_extracting
+        ))
+    except:
+        return "Something wrong"
+    return "Successfully Set the Password in the PDF"
 
 
 @app.get("/")
 def read_root():
     generatePassword()
     return 'PDF Created Successfully'
+
+@app.post("/items/")
+async def create_item(user: User):
+    if len(user.password) == 0:
+        return "Password length is zero, Please provide a valid password"
+    res = generatePassword(user.password)
+    
+    return res
 
 
